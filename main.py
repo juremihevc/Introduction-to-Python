02e49ab22278fbdfc11d2c9b561dbac64f1aeb66
Jure Mihevc <juremihevcster@gmail.com>
2021-04-05 22:19:13 +0200

    Added all the if and else statements

---
+++ main.py
@@ -0,0 +1,19 @@
+mood = input("What mood are you in? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Smile, the world is a wonderful place!.")
+
+elif mood == "excited":
+    print("I think you should lower your expectations dude...")
+
+elif mood == "relaxed":
+    print("Wake up!!!!")
+
+else:
+    print("Sorry, I dont know this kind of mood. I think you're screwd...")
