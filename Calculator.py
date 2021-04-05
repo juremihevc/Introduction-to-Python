5fca7476a344c36f20f0367775ad74c68242a7e8
Jure Mihevc <juremihevcster@gmail.com>
2021-04-05 23:06:53 +0200

    create calculator.py

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+number_a = int(input("Enter first number: "))
+operator = input("Enter operator (+ , - , * , / ): ")
+number_b = int(input("Enter second number: "))
+
+if operator == "+":
+    print(number_a + number_b)
+
+if operator == "-":
+    print(number_a - number_b)
+
+if operator == "*":
+    print(number_a * number_b)
+
+if operator == "/":
+    print(number_a / number_b)
